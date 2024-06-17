cb4657a8d9f68cbc37799c5311c8baa11296c0b0
MarcosPerez16 <marcos.perez06@yahoo.com>
2024-06-17 07:25:35 -0700

    created state for puppies list

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import "./App.css";
+import { puppyList } from "./data";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log(puppies);
   return (
     <>
       <div></div>
